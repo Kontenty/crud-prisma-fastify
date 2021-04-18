bd91f41e5ae4a8ce74d03c33f82b27ca3ece5117
Konrad Gorski <konradgrsk@gmail.com>
2021-04-18 20:15:44 +0200

    added swagger and cors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ const fastify = require("fastify");
 const pino = require("pino");
 
 const routes = require("./routes");
+const swagger = require("./config/swagger");
 
 const server = fastify({
   logger: pino({
     prettyPrint: true,
   }),
 });
+
+server.register(require("fastify-swagger"), swagger.options);
+server.register(require("fastify-cors"));
 
 server.get("/ping", {}, async (request, reply) => {
   // console.log(query);
@@ -23,6 +27,7 @@ routes.forEach((route) => {
 const start = async () => {
   try {
     await server.listen(5000);
+    server.swagger();
     const address = server.server.address();
     server.log.info(`Running on: ${JSON.stringify(address)}`);
   } catch (error) {
